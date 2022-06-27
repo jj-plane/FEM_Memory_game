f535a7bc2ad91aa6a2bcd9f6276f71320405ee73
Aggas, Joshua <Joshua.Aggas@ddiworld.com>
2022-06-27 18:10:31 -0400

    added a timer to the player display component. Probably need to rewrite this but it's good enough for now

--- src/app/player-display/player-display.component.ts
+++ src/app/player-display/player-display.component.ts
@@ -1,20 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Player } from '../player.interface';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-player-display',
   templateUrl: './player-display.component.html',
   styleUrls: ['./player-display.component.scss']
 })
-export class PlayerDisplayComponent implements OnInit {
+export class PlayerDisplayComponent implements OnInit, OnDestroy {
 
 
   @Input() multiplePlayers = false;
   @Input() currentPlayer = 0;
   @Input() players: Player[] = [];
+
+  timer?: string = undefined;
   constructor() { }
 
   ngOnInit(): void {
+    console.log('hello')
+    timer(0, 1000).subscribe(n => this.updateTimer(n));
+  }
+
+  ngOnDestroy(){
+    
+  }
+
+  updateTimer(n:number){
+    //This should probably be it's own function cause I can reuse it.
+    //I'd move it if I was working with a team.
+    this.timer = new Date(n * 1000).toISOString().substring(11, 19);
   }
 
 }
